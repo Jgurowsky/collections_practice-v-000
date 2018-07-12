2a4becbc609047f32e9387da502b8645db320698
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-12 18:37:35 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,10 +14,12 @@ end
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
+  array
 end
 
 def swap_elements_from_to(array, index, i_destination)
   array[index], array[i_destination] = array[i_destination], array[index]
+  array
 end
 
 print swap_elements(array)
